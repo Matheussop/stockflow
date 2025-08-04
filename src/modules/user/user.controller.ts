4772233444a6c92b762cfa89f2f646bce908d81f
Matheussop <matheussopluiz@outlook.com>
2025-08-03 22:41:45 -0300

    Add @nestjs/mapped-types dependency and configure Prisma seeding script. Refactor UserController import path for RolesGuard and remove obsolete user.controller.ts file.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,9 +5,9 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { User } from '../auth/decorators/user.decorator';
 import type { JwtPayload } from 'src/modules/auth/interfaces/jwt-payload.interface';
-import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Role } from '@prisma/client';
 import { Roles } from 'src/common/decorators/roles.decorator';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @Controller('users')
 export class UserController {
